21e4339ca9bc244548787a8997c136a545f2ec03
fcschmidt <fcschimidt@gmail.com>
2019-08-04 13:34:24 -0300

    created method for scraping url for xls content

---
+++ src/scraping.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+def scraping_de(url):
+    index_page = '/'.join(url.split('/')[0:3])
+
+    with urlopen(url) as u:
+        soup = BeautifulSoup(u, "html5lib")
+
+    xls_hrefs = soup.find_all("a", href=re.compile("xls"))
+    xls_file_name = [t.get("title").lower().replace(" ", "_") for t in xls_hrefs]
+    xls_ = [f"{index_page}{h.get('href')}" for h in xls_hrefs]
+
+    xls_content = []
+
+    for r in range(0, len(xls_file_name)):
+        xls_dic = {
+            "file_name": xls_file_name[r],
+            "xls_url": xls_[r]
+        }
+        xls_content.append(xls_dic)
+
+    return xls_content
